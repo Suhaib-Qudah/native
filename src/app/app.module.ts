7dfcb09bd95aba2bd91f9d0dfbba0302ac19e881
Suhaib Qudah <=>
2022-08-06 13:04:50 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    AddUserComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
